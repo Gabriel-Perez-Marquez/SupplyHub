ec2ca650731e09975f83e2b1ea78ae73d1bb1a82
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2026-01-27 11:38:47 +0100

    Proyecto creado

---
+++ src/test/java/com/salesianostriana/dam/SupplyHub/SupplyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.SupplyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
